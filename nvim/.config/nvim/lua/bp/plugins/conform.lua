c9bcc525a3cc978afd7416dbd4a5286cfce726df
bart-p <bartek.paraf@gmail.com>
2025-08-10 10:23:02 +0200

    removed format on save completelly, using the keybinding for it if needed

--- nvim/.config/nvim/lua/bp/plugins/conform.lua
+++ nvim/.config/nvim/lua/bp/plugins/conform.lua
@@ -8,13 +8,8 @@ return {
 				-- Conform will run the first available formatter
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 			},
-			-- format_on_save = {
-			-- 	-- These options will be passed to conform.format()
-			-- 	timeout_ms = 500,
-			-- 	lsp_format = "fallback",
-			-- },
 		})
-		-- ⌨️ Manual format command
+		-- Manual format command
 		vim.keymap.set("n", "<leader>cf", function()
 			require("conform").format({ async = true, lsp_fallback = true })
 		end, { desc = "Format file (Conform)" })
